3be430358419310adea87d1dfb8564834d532a3e
Marcin Szewczyk <marcin.szewczyk@wodny.org>
2015-12-18 13:52:27 +0100

    add counter

--- crash.c
+++ crash.c
@@ -7,18 +7,20 @@ int main(int argc, char **argv) {
     if (stdin_ch == NULL)
         error (1, 0, "failed to create channel");
 
+    unsigned int i = 0;
     gchar *filename;
     gsize terminator;
     GdkPixbufAnimation *anim;
     while (g_io_channel_read_line (stdin_ch, &filename, NULL, &terminator, NULL) == G_IO_STATUS_NORMAL) {
         filename[terminator] = '\0';
-        printf ("filename: %s\n", filename);
+        printf ("%3u filename: %s\n", i, filename);
         anim = gdk_pixbuf_animation_new_from_file (filename, NULL);
         if (anim == NULL)
             error (1, 0, "failed to create animation");
         g_free (filename);
         // the killing machine
         //g_object_unref (anim);
+        ++i;
     }
 
     return 0;
